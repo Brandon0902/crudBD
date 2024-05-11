1ad52e84cadc0088ff2f042f6069ea0e37c931e4
Brandon Lopez <brandonaxellopezdealba@gmail.com>
2024-05-11 14:58:24 -0600

    changed provider this vaule

--- app/Providers/AppServiceProvider.php
+++ app/Providers/AppServiceProvider.php
@@ -20,7 +20,7 @@ class AppServiceProvider extends ServiceProvider
      */
     public function boot(): void
     {
-        if (this->app->eviroment('production')) {
+        if ($this->app->evironment('production')) {
             URL::forceSchema('https');
         }
     }
